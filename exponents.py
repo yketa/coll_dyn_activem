8daa7f21ee8c9bdea4cf8150df5ed076b52a24e8
Yann-Edwin Keta <yann-edwin.keta@ens-lyon.fr>
2020-09-22 21:21:02 +0200

    numerical error handling

--- exponents.py
+++ exponents.py
@@ -74,6 +74,7 @@ def float_to_letters(flo):
 	"""
 
 	try:
+		flo = flo.__round__(4)
 		expo_int = int(floor(log10(abs(flo))))			# integer exponent
 		expo_let = list(_exponents.keys())[list(_exponents.values()).index(
 			'%i' % expo_int								# corresponding letter exponent
